6f9b0872c56e49240e2f8c1416f125e6b2cb0f43
Adrian <adriandurran@users.noreply.github.com>
2019-10-25 20:44:57 +0100

    server - testing out responses

--- server/src/controllers/socialController.js
+++ server/src/controllers/socialController.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
 export const getSocialAccounts = async (req, res) => {
-  console.log(req.params);
+  console.log(req.params.name);
+  try {
+    const result = await axios.get(`https://www.github.com/${req.params.name}`);
+
+    let rawData = '';
+    // result.on('data', (chunk) => {
+    //   rawData += chunk;
+    // });
+    // result.on('end', () => {
+    //   rawData = rawData.replace(/(<([^>]+)>)/gi, '');
+    //   console.log(rawData);
+    // });
+    rawData = result.data.replace(/(<([^>]+)>)/gi, '');
+    res.send(rawData);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
 };
