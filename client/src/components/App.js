45f890ee7b5bea4c6c2f34ac5b1ddfe7f5ada71e
Adrian <adriandurran@users.noreply.github.com>
2019-11-05 13:48:46 +0000

    client - restructure of folders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import '../css/App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header"> Twitter Search</header>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// dev only
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(reduxThunk))
+);
+
+// prod only
+// const store = createStore(reducers, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
