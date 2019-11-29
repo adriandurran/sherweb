9416571908f6873c9fe4131723aaa8d5dbb3d41e
Adrian Durran <adrian@intellidroid.eu>
2019-11-29 12:38:38 +0000

    client - testing the model

--- client/src/utils/toxicChecker.js
+++ client/src/utils/toxicChecker.js
@@ -13,12 +13,13 @@ export const runToxicityCheck = async (tweets) => {
   for (let i = 0; i < 10; i++) {
     tweetArray.push(tweets[i].full_text);
     let results = await checkToxicText(tweetArray, threshold);
+    // need to put back into the tweet object and update redux
     console.log(results);
   }
 };
 
 const checkToxicText = async (text, threshold) => {
-  const sentences = text;
+  const sentences = [text];
 
   try {
     const model = await toxicity.load(threshold);
