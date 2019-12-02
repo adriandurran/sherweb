8145c6e3dfbcdcd589adab01e7c496956911c859
Adrian <adriandurran@users.noreply.github.com>
2019-12-02 09:40:03 +0000

    client - optimised the toxicity check

--- client/src/actions/twitter.js
+++ client/src/actions/twitter.js
@@ -30,18 +30,15 @@ export const fetchMoreResults = (query) => async (dispatch) => {
   // query is equal to the next string
 };
 
-// take results
-// run through model
-// add to results
-// update redux store
-export const runToxicityCheck = (tweets) => async (dispatch) => {
-  const threshold = 0.9;
+export const runToxicityCheck = (tweets, threshold = 0.9) => async (
+  dispatch
+) => {
   const tweetLength = tweets.length;
-
   const tweetArray = [];
-
+  const model = await toxicity.load(threshold);
+  // need to consider different language versions!!!!!!
   for (let i = 0; i < tweetLength; i++) {
-    let results = await checkToxicText(tweets[i].full_text, threshold);
+    let results = await model.classify(tweets[i].full_text);
     // add it to the tweet object
     let tempObj = { ...tweets[i], toxicity: results };
     // need to put back into the tweet array and
@@ -49,16 +46,4 @@ export const runToxicityCheck = (tweets) => async (dispatch) => {
   }
   // update redux
   dispatch({ type: ADD_TWITTER_TOXICITY, payload: tweetArray });
-};
-
-const checkToxicText = async (text, threshold) => {
-  const sentences = [text];
-
-  try {
-    const model = await toxicity.load(threshold);
-    const predictions = await model.classify(sentences);
-    return predictions;
-  } catch (error) {
-    return null;
-  }
 };
