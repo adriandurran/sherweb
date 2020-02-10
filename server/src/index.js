f338b4b8f1be37cc2521a899b3b1c58157501817
Adrian Durran <adrian@intellidroid.eu>
2020-02-10 16:28:05 +0000

    application - adding in keycloak

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,31 @@
 import express from 'express';
+import Keycloak from 'keycloak-connect';
+import session from 'express-session';
 import morgan from 'morgan';
 
 import socialRoutes from './routes/socialRoutes';
 import twitterRoutes from './routes/twitterRoutes';
 
-const app = express();
+const memoryStore = new session.MemoryStore();
 
+const app = express();
+app.use(
+  session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  })
+);
+const keycloak = new Keycloak({ store: memoryStore });
+app.use(keycloak.middleware());
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/social', socialRoutes);
 app.use('/api/twitter', twitterRoutes);
+
+app.get('/test', keycloak.protect(), (req, res) => res.send('hello'));
 
 const PORT = process.env.PORT || 4050;
 
